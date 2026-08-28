88efde2d6bdd902234718c834c18b5f179d046a5
BeluGerinTS <elvanli520@gmail.com>
2026-08-28 22:29:22 +0700

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Topbar } from './topbar/topbar';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
+import { NavMenu } from './nav-menu/nav-menu';
+import { HeroHeader } from './hero-header/hero-header';
+import { Projects } from './projects/projects';
+import { Services } from './services/services';
+import { Footer } from './footer/footer';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet, 
+    Topbar, Home, About, Contact, 
+    NavMenu, HeroHeader, Projects, Services, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('my-app');
+  title = 'Pertemuan_2';
 }
